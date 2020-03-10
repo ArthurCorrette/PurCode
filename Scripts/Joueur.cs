630d08bb6bdb5f50bdb430f09cbcde2ca967c57d
Arthur Corrette <arthur.corrette@gmail.com>
2020-03-10 17:39:18 +0100

    changement de dossier

---
+++ Scripts/Joueur.cs
@@ -0,0 +1,40 @@
+public class Joueur
+{
+	// ça marche pour pitot
+    private De de;
+	public int PtsDeVies { get; private set; }
+	public bool EstVivant
+	{
+		get { return PtsDeVies > 0; }
+	}
+
+	public Joueur(int points)
+	{
+		PtsDeVies = points;
+		de = new De();
+	}
+
+	public void Attaque(MonstreFacile monstre)
+	{
+		int lanceJoueur = LanceLeDe();
+		int lanceMonstre = monstre.LanceLeDe();
+		if (lanceJoueur >= lanceMonstre)
+			monstre.SubitDegats();
+	}
+
+	public int LanceLeDe()
+	{
+		return de.LanceLeDe();
+	}
+
+	public void SubitDegats(int degats)
+	{
+		if (!BouclierFonctionne())
+			PtsDeVies -= degats;
+	}
+
+	private bool BouclierFonctionne()
+	{
+		return de.LanceLeDe() <= 2;
+	}
+}
